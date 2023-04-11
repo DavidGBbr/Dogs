75efcbce271d4a015204be1573d892f708e7b621
davidGBbr <davidgomesbrigido@gmail.com>
2023-04-11 12:09:47 -0300

    Dogs - #005

--- src/Components/Forms/Input/index.js
+++ src/Components/Forms/Input/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import * as C from "./styles";
 
-export const Input = ({ label, type, name }) => {
+export const Input = ({
+  label,
+  type,
+  name,
+  value,
+  onChange,
+  error,
+  onBlur,
+}) => {
   return (
     <C.Wrapper>
       <C.Label htmlFor={name}>{label}</C.Label>
-      <C.Input id={name} name={name} type={type} />
-      <C.Error>Error</C.Error>
+      <C.Input
+        id={name}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <C.Error>{error}</C.Error>}
     </C.Wrapper>
   );
 };
