09b584f02e35c5d1f4bdb269a4d85542a45b9afc
davidGBbr <davidgomesbrigido@gmail.com>
2023-04-11 16:48:25 -0300

    Dogs - #007

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import * as C from "./styles";
-import { Link } from "react-router-dom";
 import { ReactComponent as Dogs } from "../../Assets/dogs.svg";
+import { UserContext } from "../../Contexts/UserContext";
 
 export const Header = () => {
+  const { data } = useContext(UserContext);
   return (
     <C.Header>
       <C.Nav className="container">
         <C.Logo to="/" aria-label="Dogs - Home">
           <Dogs />
         </C.Logo>
-        <C.Login to="/login">Login / Criar</C.Login>
+        {data ? (
+          <C.Login to="/conta">{data.nome}</C.Login>
+        ) : (
+          <C.Login to="/login">Login / Criar</C.Login>
+        )}
       </C.Nav>
     </C.Header>
   );
