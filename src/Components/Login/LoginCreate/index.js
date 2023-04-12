8533142cb7b8c5d7ac00291363b5bae504c26fa9
davidGBbr <davidgomesbrigido@gmail.com>
2023-04-12 19:08:04 -0300

    Dogs - #011

--- src/Components/Login/LoginCreate/index.js
+++ src/Components/Login/LoginCreate/index.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Input } from "../../Forms/Input";
+import { Button } from "../../Forms/Button";
+import { useForm } from "../../../Hooks/useForm";
+import { USER_POST } from "../../../api";
+import { UserContext } from "../../../Contexts/UserContext";
 
 export const LoginCreate = () => {
-  return <div>LoginCreate</div>;
+  const username = useForm(UserContext);
+  const email = useForm("email");
+  const password = useForm("password");
+
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+    console.log(response.ok);
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha um email válido",
   },
+  password: {
+    regex: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{9,}$/,
+    message:
+      "A senha precisa ter 1 caracter maísculo, 1 minúsculo e 1 digito. Com no mínimo 9 caracteres.",
+  },
 };
 
 export const useForm = (type) => {
