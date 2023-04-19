6a788d13bded9c0968d0f99894789e0fa5c77322
davidGBbr <davidgomesbrigido@gmail.com>
2023-04-19 18:39:12 -0300

    Dogs - #024

--- src/Components/Feed/index.js
+++ src/Components/Feed/index.js
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as C from "./styles";
 import { FeedModal } from "./FeedModal";
 import { FeedPhotos } from "./FeedPhotos";
 import { useState } from "react";
 
-export const Feed = () => {
+export const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = useState(null);
+  const [pages, setPages] = useState([1]);
+  const [infinite, setInfinite] = useState(true);
+
+  useEffect(() => {
+    let wait = false;
+    function infiniteScroll(event) {
+      if (infinite) {
+        const scroll = window.scrollY;
+        const height = document.body.offsetHeight - window.innerHeight;
+        if (scroll > height * 0.75 && !wait) {
+          setPages((pages) => [...pages, pages.length + 1]);
+          wait = true;
+          setTimeout(() => {
+            wait = false;
+          }, 500);
+        }
+      }
+    }
+
+    window.addEventListener("wheel", infiniteScroll);
+    window.addEventListener("scroll", infiniteScroll);
+    return () => {
+      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener("scroll", infiniteScroll);
+    };
+  }, []);
 
   return (
     <div>
       {modalPhoto && (
         <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
       )}
-      <FeedPhotos setModalPhoto={setModalPhoto} />
+      {pages.map((page) => (
+        <FeedPhotos
+          key={page}
+          user={user}
+          page={page}
+          setModalPhoto={setModalPhoto}
+          setInfinite={setInfinite}
+        />
+      ))}
     </div>
   );
 };
--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { UserHeader } from "./UserHeader";
 import { Feed } from "../Feed";
 import { Route, Routes } from "react-router-dom";
 import { UserPhotoPost } from "./UserPhotoPost";
 import { UserStats } from "./UserStats";
+import { UserContext } from "../../Contexts/UserContext";
 
 export const User = () => {
+  const { data } = useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
       </Routes>
