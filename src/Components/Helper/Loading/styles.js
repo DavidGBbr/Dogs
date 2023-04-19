f5b74b7c27ee3653be09ea071696c4c9ed152e51
davidGBbr <davidgomesbrigido@gmail.com>
2023-04-18 21:45:49 -0300

    Dogs - #023

---
+++ src/Components/Helper/Loading/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  top: 0px;
+  left: 0px;
+  z-index: 1000;
+`;
+
+export const Loading = styled.div`
+  margin: auto;
+  width: 80px;
+  height: 80px;
+  display: flex;
+  padding-left: 5px;
+  background: rgba(255, 255, 255, 0.5);
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+`;
