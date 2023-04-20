d363498465d506693fb1f890ea44516df1b19751
davidGBbr <davidgomesbrigido@gmail.com>
2023-04-20 14:26:44 -0300

    Dogs - #032

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import * as C from "./styles";
+import { ReactComponent as Dogs } from "../../Assets/dogs-footer.svg";
 
 export const Footer = () => {
-  return <C.Footer>Footer</C.Footer>;
+  return (
+    <C.Footer>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </C.Footer>
+  );
 };
--- src/Components/Footer/styles.js
+++ src/Components/Footer/styles.js
@@ -1,3 +1,13 @@
 import styled from "styled-components";
 
-export const Footer = styled.footer``;
+export const Footer = styled.footer`
+  background: #fb1;
+  padding: 3rem 1rem 0 1rem;
+  height: 10rem;
+  text-align: center;
+  color: #764701;
+
+  p {
+    margin-top: 1rem;
+  }
+`;
